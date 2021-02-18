f71bc31ef1bddecfb576b95191cd821e0cd30dff
jyk <koolucy97@gmail.com>
2021-02-18 10:37:21 +0900

    test - home , DB값 받아오기

--- src/main/java/com/hodu/controller/TestController.java
+++ src/main/java/com/hodu/controller/TestController.java
@@ -1,15 +1,22 @@
 package com.hodu.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import com.hodu.service.TestService;
+
 @Controller
 public class TestController {
-	
+	@Autowired
+	TestService test;
 
 	@GetMapping(value = "test")
 	public void test(Model model) {
+		
+		model.addAttribute("test1",test.TnameInformation());
+		
 	}
 
 }
--- src/main/webapp/WEB-INF/views/test.jsp
+++ src/main/webapp/WEB-INF/views/test.jsp
@@ -1,14 +1,16 @@
-<%@ page language="java" contentType="text/html; charset=EUC-KR"
-    pageEncoding="EUC-KR"%>
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
 <!DOCTYPE html>
 <html>
 <head>
-<meta charset="EUC-KR">
+<meta charset="UTF-8">
 <title>Insert title here</title>
 </head>
 <body>
 
-
 <h1> TEST </h1>
+<button onclick="location.href='./'">버튼</button>
+${test1 }
 </body>
 </html>
