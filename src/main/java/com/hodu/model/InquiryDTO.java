db951d0194117f04f7668b77816f637685c58d7b
jihyung <jihyung@LAPTOP-AFHHG7TK>
2021-02-19 15:46:04 +0900

    DTO

---
+++ src/main/java/com/hodu/model/InquiryDTO.java
@@ -0,0 +1,20 @@
+package com.hodu.model;
+
+import java.util.Date;
+
+import lombok.Data;
+import oracle.sql.BLOB;
+
+@Data
+public class InquiryDTO {
+	private Integer inquiry_postnum;
+	private String member_id;
+	private String inquiry_group;
+	private String inquiry_title;
+	private String inquiry_content;
+	private BLOB inquiry_img;
+	private Date inquiry_date;
+	private Integer inquiry_views;
+	private String inquiry_secret;
+	private String inquiry_show;
+}
---
+++ src/main/java/com/hodu/model/QuestionDTO.java
@@ -0,0 +1,20 @@
+package com.hodu.model;
+
+import java.util.Date;
+
+import lombok.Data;
+
+@Data
+public class QuestionDTO {
+	
+	private Integer question_postnum;
+	private String member_id;
+	private String item_code;
+	private String question_title;
+	private String question_content;
+	private Date question_date;
+	private Integer question_views;
+	private String question_secret;
+	private String question_show;
+
+}
