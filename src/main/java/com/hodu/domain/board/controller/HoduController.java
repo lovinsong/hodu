5525bfe309d882cc5204248896115eb4cb93253f
jyk <koolucy97@gmail.com>
2021-02-28 15:05:34 +0900

    board에 controller 삽입 완료

--- src/main/java/com/hodu/domain/board/controller/HoduController.java
+++ src/main/java/com/hodu/domain/board/controller/HoduController.java
@@ -1,4 +1,4 @@
-package com.hodu.controller;
+package com.hodu.domain.board.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
