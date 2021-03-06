a53f54d9c036066086b78119324c07d2332ab6c3
MANGI <lovinsong@kakao.com>
2021-03-06 18:08:21 +0900

    3.6

---
+++ src/main/java/com/hodu/domain/payment/controller/paymentController.java
@@ -0,0 +1,27 @@
+package com.hodu.domain.payment.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import lombok.extern.log4j.Log4j;
+
+@Controller
+@Log4j
+public class paymentController {
+
+	@GetMapping(value = "/hodu/payment/shoppingbasket")
+	public void basketGET() {
+		
+	}
+
+	@GetMapping(value = "/hodu/payment/payment")
+	public void paymentGET() {
+		
+	}
+	
+	@GetMapping(value = "/hodu/payment/okpayment")
+	public void okPaymentGET() {
+		
+	}
+	
+}
